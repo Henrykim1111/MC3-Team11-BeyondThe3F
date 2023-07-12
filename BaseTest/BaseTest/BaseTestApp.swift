6e0e0dff95fa9a6416711d574478769039fa19bb
Moonkey <moonkey48@icloud.com>
2023-07-12 16:11:04 +0900

    [Setting] #13 - BaseTest 프로젝트 생성

---
+++ BaseTest/BaseTest/BaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseTestApp.swift
+//  BaseTest
+//
+//  Created by Seungui Moon on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct BaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
