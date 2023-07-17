a77140686bb676ad52cb23ac13feb3842b8c1697
Moonkey <moonkey48@icloud.com>
2023-07-17 15:47:42 +0900

    [Setting] #18 - MC3-BeyondThe3F 프로젝트 개설

---
+++ MC3-BeyondThe3F/MC3-BeyondThe3F/MC3_BeyondThe3FApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_BeyondThe3FApp.swift
+//  MC3-BeyondThe3F
+//
+//  Created by Seungui Moon on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct MC3_BeyondThe3FApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
