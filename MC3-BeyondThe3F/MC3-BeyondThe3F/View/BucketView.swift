42dd21ea506aebea536bdccdced73c8d9f2d0349
Moonkey <moonkey48@icloud.com>
2023-07-18 12:14:42 +0900

    [Feat] #21 - TabView 생성 및 BucketView, MapView 생성

---
+++ MC3-BeyondThe3F/MC3-BeyondThe3F/View/BucketView.swift
@@ -0,0 +1,20 @@
+//
+//  BucketView.swift
+//  MC3-BeyondThe3F
+//
+//  Created by Seungui Moon on 2023/07/18.
+//
+
+import SwiftUI
+
+struct BucketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BucketView_Previews: PreviewProvider {
+    static var previews: some View {
+        BucketView()
+    }
+}
