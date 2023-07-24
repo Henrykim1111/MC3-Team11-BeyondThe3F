1f135afea15663361c827592fef639be0fd14981
Hanyeonggyun <hyg100@naver.com>
2023-07-24 17:56:21 +0900

    음악 추가 리스트 뷰 구현

--- MC3-BeyondThe3F/MC3-BeyondThe3F/View/AddMusicView.swift
+++ MC3-BeyondThe3F/MC3-BeyondThe3F/View/AddMusicView.swift
@@ -7,32 +7,79 @@
 
 import SwiftUI
 
+enum AddMusicItemData: CaseIterable {
+    case music
+    case location
+    case date
+    
+    var description: String {
+        switch self {
+        case .music:
+            return "음악"
+        case .location:
+            return "위치"
+        case .date:
+            return "날짜"
+        }
+    }
+    
+    
+    
+    
+    var destination : some View {
+        Group {
+            switch self {
+            case .music:
+                MainTabView()
+            case .location:
+                EditMapPositionView()
+            case .date:
+                EditDateView()
+            }
+        }
+    }
+}
+
+struct MusicNameRow: View {
+    @State var itemData: AddMusicItemData
+    
+    
+    var body: some View {
+        NavigationLink {
+            itemData.destination
+        } label: {
+            Text("\(itemData.description)")
+        }
+    }
+}
 struct AddMusicView: View {
     var body: some View {
         NavigationStack{
             VStack{
-                Spacer()
-                NavigationLink {
-                    MainTabView()
-                       
-                } label: {
-                    
-                    PrimaryButtonComponentView(buttonType: .forSave, backgroundColor: .primary)
-                }
-                .navigationTitle("음악 추가")
-                
+                    List{
+                        ForEach(AddMusicItemData.allCases, id: \.self) { addCase in
+                            MusicNameRow(itemData: addCase)
+                        }
+                    }
+                    Spacer()
+                    NavigationLink {
+                        MainTabView()
+                    } label: {
+                        PrimaryButtonComponentView(buttonType: .forSave, backgroundColor: .primary)
+                    }
+                    .navigationTitle("음악 추가")
             }
+            
+            
+            
+            
+            
         }
-        
-        
-       
-
-        
     }
-}
-
-struct AddMusicView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddMusicView()
+    
+    struct AddMusicView_Previews: PreviewProvider {
+        static var previews: some View {
+            AddMusicView()
+        }
     }
 }
