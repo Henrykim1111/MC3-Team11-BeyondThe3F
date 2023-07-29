d86212b8ec133463e95480ebe27c3b4fe0bbc62b
aewols <i0y0j057@gmail.com>
2023-07-29 17:04:12 +0900

    [Feat] #103 - MusicPlayerComponentView 디테일 수정

--- MC3-BeyondThe3F/MC3-BeyondThe3F/View/Components/MusicPlayerComponentView.swift
+++ MC3-BeyondThe3F/MC3-BeyondThe3F/View/Components/MusicPlayerComponentView.swift
@@ -6,41 +6,83 @@
 //
 
 import SwiftUI
+import MusicKit
 
 struct MusicPlayerComponentView: View {
+    
+    @ObservedObject private var musicPlayer = MusicPlayer.shared
+    @State private var isPlaying = false
+    @State private var showMusicPlayListView = false
+    
+    
     var body: some View {
-        HStack{
-            Spacer()
-                .frame(width: 16)
-            Rectangle()
+        HStack {
+            Image("musicPlayImageEmpty")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
                 .frame(width: 60, height: 60)
                 .cornerRadius(8)
+            
             Spacer()
                 .frame(width: 16)
-            VStack(alignment:.leading){
-                Text("Title")
-                    .body1(color: .white)
-                    .padding(.bottom, 8)
-                Text("subtitle1")
-                    .body2(color: .gray500)
+            
+            VStack(alignment: .leading) {
+                if let currentMusicItem = musicPlayer.currentMusicItem {
+                    Text("\(currentMusicItem.songName ?? "")")
+                        .body1(color: .white)
+                        .truncationMode(.tail)
+                    Spacer()
+                        .frame(height: 6)
+                    Text("\(currentMusicItem.artistName ?? "")")
+                        .body2(color: .gray500)
+                        .truncationMode(.tail)
+                } else {
+                    Text("재생 중이 아님")
+                        .body1(color: .white)
+                        .truncationMode(.tail)
+                }
             }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .frame(height: 48)
+            
             Spacer()
-            HStack(spacing: 20) {
-                SFImageComponentView(symbolName: .play, color: .white, width: 20)
-                SFImageComponentView(symbolName: .forward, color: .white, width: 35)
-                SFImageComponentView(symbolName: .list, color: .white)
+                .frame(width: 16)
+            
+            HStack(spacing: 24) {
+                Button {
+                    musicPlayer.playButtonTapped()
+                    isPlaying.toggle()
+                } label: {
+                    SFImageComponentView(symbolName: isPlaying ? .play : .pause, color: .white, width: 20)
+                }
+                
+                Button {
+                    musicPlayer.nextButtonTapped()
+                } label: {
+                    SFImageComponentView(symbolName: .forward, color: .white, width: 32)
+                }
+                .disabled(musicPlayer.isLast)
+                
+                Button {
+                    showMusicPlayListView = true
+                } label: {
+                    SFImageComponentView(symbolName: .list, color: .white)
+                }
             }
-            Spacer()
-                .frame(width: 10)
         }
+        .padding(.horizontal, 20)
         .foregroundColor(.white)
         .frame(width: 390, height: 88)
         .background(Color.custom(.secondaryDark))
+        .sheet(isPresented: $showMusicPlayListView) {
+            MusicPlayView()
+                .presentationDragIndicator(.visible)
+        }
     }
 }
 
-struct MusicPlayerComponentView_Previews: PreviewProvider {
-    static var previews: some View {
-        MusicPlayerComponentView()
-    }
-}
+//struct MusicPlayerComponentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MusicPlayerComponentView()
+//    }
+//}
