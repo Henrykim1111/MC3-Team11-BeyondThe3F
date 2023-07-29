185e009244163aa79b201151c47f75373c307bf5
Moonkey <moonkey48@icloud.com>
2023-07-29 17:28:01 +0900

    [Feat] #122 - WTEditMusicView 생성

--- MC3-BeyondThe3F/MC3-BeyondThe3F/View/WorkThrough/WTEditMusicView.swift
+++ MC3-BeyondThe3F/MC3-BeyondThe3F/View/WorkThrough/WTEditMusicView.swift
@@ -1,5 +1,5 @@
 //
-//  WTEditDateView.swift
+//  WTEditMusicView.swift
 //  MC3-BeyondThe3F
 //
 //  Created by Seungui Moon on 2023/07/29.
@@ -7,21 +7,22 @@
 
 import SwiftUI
 
-struct WTEditDateView: View {
+struct WTEditMusicView: View {
     var body: some View {
         VStack {
             Text("map edit position")
             NavigationLink {
-                WTEditMusicView()
+                MainTabView()
+                    .navigationBarBackButtonHidden(true)
             } label: {
-                Text("이 날짜로 저장")
+                Text("저장")
             }
         }
     }
 }
 
-struct WTEditDateView_Previews: PreviewProvider {
+struct WTEditMusicView_Previews: PreviewProvider {
     static var previews: some View {
-        WTEditDateView()
+        WTEditMusicView()
     }
 }
