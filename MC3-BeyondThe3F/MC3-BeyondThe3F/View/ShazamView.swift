7afada48952f0fe3588eaaf736f25c691041f324
Moonkey <moonkey48@icloud.com>
2023-08-01 09:38:43 +0900

    [Fix] #157 - ShazamView 음악 재생 기능 제외하고 구현

--- MC3-BeyondThe3F/MC3-BeyondThe3F/View/ShazamView.swift
+++ MC3-BeyondThe3F/MC3-BeyondThe3F/View/ShazamView.swift
@@ -15,6 +15,7 @@ private enum ShazamResultType {
 }
 
 struct ShazamView: View {
+    @Environment(\.dismiss) private var dismiss
     @StateObject private var shazamViewModel = ShazamViewModel()
     @State private var musicName = "music name"
     @State private var artistName = "artist name"
@@ -26,6 +27,9 @@ struct ShazamView: View {
     @State private var circleScaleBig: CGFloat = 1
     private let circleAnimationSmall = Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)
     private let circleAnimationBig = Animation.easeInOut(duration: 1).repeatForever(autoreverses: true).delay(0.1)
+    
+    let musicItemUpdateViewModel = MusicItemUpdateViewModel.shared
+    let musicPlayer = MusicPlayer.shared
     
     var body: some View {
         VStack {
@@ -202,7 +206,8 @@ extension ShazamView {
             Spacer()
                 .frame( height: 10)
             Button {
-                // TODO: restart
+                self.currentState = .listening
+                shazamViewModel.startRecognition()
             } label: {
                 HStack{
                     SFImageComponentView(symbolName: .arrowCounterClockwise, color: .white, width: 21, height: 24)
@@ -240,11 +245,12 @@ extension ShazamView {
                 }      
                 Spacer()
                 Button {
-                    // TODO: add to music play list the music Id
-                    guard let appleMusicId = self.musicId else {
-                        return
-                    }
-                    
+                    // TODO: musicPlay에 음악을 추가할 때 이미지를 전달해주지 않아서 플레이리스트에 나오지 않는 것으로 추측
+                    // insertMusicAndPlay 점검 필요
+//                    guard let appleMusicId = self.musicId else {
+//                        return
+//                    }
+//
                 } label: {
                     ButtonPlayComponentView()
                         .padding(2)
@@ -256,42 +262,57 @@ extension ShazamView {
             VStack {
                 Spacer()
                     .frame(height: 200)
-                Button {
-                    self.currentState = .listening
-                    shazamViewModel.startRecognition()
-                } label: {
-                    ZStack{
-                        HStack(spacing: 12){
-                            Button {
-                                // TODO: restart
-                            } label: {
-                                HStack(spacing: 0) {
-                                    SFImageComponentView(symbolName: .arrowCounterClockwise, color: .white, width: 21, height: 20)
-                                    Text("다시 검색")
-                                        .body2(color: .white)
-                                }
-                            }
-                            Divider()
-                                .frame(height: 20)
-                                .background(Color.white)
-                            Button {
-                                // TODO: to editmap position
-                            } label: {
-                                HStack(spacing: 0) {
-                                    SFImageComponentView(symbolName: .plus, color: .white, width: 21, height: 20)
-                                    Text("추가 하기")
+                
+                ZStack{
+                    HStack(spacing: 12){
+                        Button {
+                            self.currentState = .listening
+                            shazamViewModel.startRecognition()
+                        } label: {
+                            HStack {
+                                SFImageComponentView(symbolName: .arrowCounterClockwise, color: .white, width: 21, height: 20)
+                                Text("다시 검색")
                                     .body2(color: .white)
-                                }
                             }
                         }
-                        .frame(width: 248, height: 35)
-                        .padding(.horizontal,5)
-                        .background(
-                            LinearGradient(gradient: Gradient(colors: [Color.custom(.primary).opacity(0), Color.custom(.secondary).opacity(1)]), startPoint: .bottom, endPoint: .top))
-                        .cornerRadius(100)
-                        
+                        Divider()
+                            .frame(height: 20)
+                            .background(Color.white)
+                        Button {
+                            guard let id = musicId else {
+                                return
+                            }
+                            Task {
+                                guard let musicItems = await MusicItemDataModel.shared.getInfoByMusicId(id) else {
+                                    return
+                                }
+                                guard let musicItem = musicItems.items.first else {
+                                    return
+                                }
+                                MusicItemUpdateViewModel.shared.musicItemshared.musicId = musicItem.id.rawValue
+                                MusicItemUpdateViewModel.shared.musicItemshared.songName = musicItem.title
+                                MusicItemUpdateViewModel.shared.musicItemshared.artistName = musicItem.artistName
+                                if let imageURL = musicItem.artwork?.url(width: 500, height: 500) {
+                                    MusicItemUpdateViewModel.shared.musicItemshared.savedImage = "\(imageURL)"
+                                } else {
+                                    MusicItemUpdateViewModel.shared.musicItemshared.savedImage = nil
+                                }
+                                musicItemUpdateViewModel.isUpdate = true
+                                dismiss()
+                            }
+                        } label: {
+                            HStack {
+                                SFImageComponentView(symbolName: .plus, color: .white, width: 21, height: 20)
+                                Text("추가 하기")
+                                .body2(color: .white)
+                            }
+                        }
                     }
-                    
+                    .frame(width: 248, height: 35)
+                    .padding(.horizontal,5)
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color.custom(.primary).opacity(0), Color.custom(.secondary).opacity(1)]), startPoint: .bottom, endPoint: .top))
+                    .cornerRadius(100)
                 }
             }
         }
